709ce0592fcf1bcd1141a5f7635791fa4552ac3e
eunbij <sareun@naver.com>
2022-03-22 19:10:44 +0900

    20220322 B-3

---
+++ b-3/script/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    // 메뉴 슬라이드
+    $('.submenu').stop().hide();
+    $('.nav li').mouseover(function(){
+        $(this).find('.submenu').stop().slideDown();
+    });
+    $('.nav li').mouseout(function(){
+        $(this).find('.submenu').stop().slideUp();
+    });
+
+    // 상하 이미지 슬라이드
+    setInterval(function(){
+        $('.slide').stop().animate({marginTop:'-300px'},function(){
+            $('.slide li').eq(0).appendTo('.slide');
+            $('.slide').css({marginTop:0});
+        });
+    },2000);
+
+    // PopUp
+    $('.popup').stop().hide();
+    $('.slide li').eq(0).click(function(){
+        $('.popup').stop().fadeIn('fast');
+    });
+    $('.p_btn').click(function(){
+        $('.popup').stop().fadeOut('fast');
+    });
+});
